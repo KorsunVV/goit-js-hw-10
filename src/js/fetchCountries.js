cb5dc11d49c394d5191719d71ef636ee1ba34ff5
KorsunVV <korsunvv.work@gmail.com>
2022-05-03 03:29:50 +0300

    final?
    end

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+const ENTRY_POINT = 'https://restcountries.com/v3.1';
+
+export function fetchCountries(countryName) {
+  return fetch(
+    `${ENTRY_POINT}/name/${countryName}?fields=name,capital,population,flags,languages`,
+  ).then(r => {
+      if (r.ok) {
+      return r.json();
+    } else {
+      throw new Error(r.status);
+    }
+  });
+}
